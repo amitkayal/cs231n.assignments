909e7c16ca1e9ed82685f659c29bf24249ffdcc9
Anand Saha <anandsaha@gmail.com>
2017-10-15 19:24:58 +0530

    svm.ipynb completed

--- 2016winter/assignment1/cs231n/classifiers/linear_classifier.py
+++ 2016winter/assignment1/cs231n/classifiers/linear_classifier.py
@@ -34,7 +34,7 @@ class LinearClassifier(object):
 
     # Run stochastic gradient descent to optimize W
     loss_history = []
-    for it in xrange(num_iters):
+    for it in range(num_iters):
       X_batch = None
       y_batch = None
 
@@ -43,13 +43,15 @@ class LinearClassifier(object):
       # Sample batch_size elements from the training data and their           #
       # corresponding labels to use in this round of gradient descent.        #
       # Store the data in X_batch and their corresponding labels in           #
-      # y_batch; after sampling X_batch should have shape (dim, batch_size)   #
+      # y_batch; after sampling X_batch should have shape (batch_size, dim)   #
       # and y_batch should have shape (batch_size,)                           #
       #                                                                       #
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      mask = np.random.choice(num_train, batch_size)
+      X_batch = X[mask, :]
+      y_batch = y[mask]
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -63,7 +65,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W = self.W - learning_rate * grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -91,7 +93,8 @@ class LinearClassifier(object):
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    scores = X.dot(self.W)
+    y_pred = np.argmax(scores, axis=1)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- 2016winter/assignment1/cs231n/classifiers/linear_svm.py
+++ 2016winter/assignment1/cs231n/classifiers/linear_svm.py
@@ -25,23 +25,53 @@ def svm_loss_naive(W, X, y, reg):
   num_classes = W.shape[1]
   num_train = X.shape[0]
   loss = 0.0
-  for i in xrange(num_train):
+  for i in range(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
-    for j in xrange(num_classes):
+    incr = 0
+    
+    for j in range(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        incr += 1
+        dW[:, j] += X[i].T # Incorrect class
         loss += margin
-
+  
+    dW[:, y[i]] += -incr * X[i].T # Correct class
+   
+    """
+    # This works, was to try out
+    for j in range(num_classes):
+      xi = X[i] # 1 x 3073
+      if j == y[i]:
+        # Gradient for correct class
+        scale = 0
+        
+        for j1 in range(num_classes):
+          if j1 == y[i]:
+            continue
+          margin = scores[j1] - correct_class_score + 1 # note delta = 1
+          if margin > 0:
+            scale += 1
+            
+        dW[:, j] += (-1) * scale * xi.T
+      else:
+        # Gradient for rest
+        scale = 0
+        margin = scores[j] - correct_class_score + 1 # note delta = 1
+        if margin > 0:
+            scale = 1
+        dW[:, j] += scale * xi.T
+    """
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
-
+  dW = dW/num_train + reg*W
   #############################################################################
   # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
@@ -50,7 +80,8 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
+  
+  # Done, see above
 
   return loss, dW
 
@@ -63,13 +94,28 @@ def svm_loss_vectorized(W, X, y, reg):
   """
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
+  num_train = X.shape[0]
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+
+  
+  scores_all = X.dot(W)
+  scores_correct = scores_all[np.arange(len(scores_all)), y].reshape((1, -1)).T
+  scores = scores_all - scores_correct + 1
+  scores[scores < 0] = 0
+  scores[np.arange(len(scores)), y] = 0
+  # Now the scores matrix is num_train x num_classes
+  # For each row, the correct class will have value of 0. Any -ve score also 
+  # would have turned to 0.
+  # Update correct class:
+  
+    
+  loss = np.sum(scores) / num_train
+  loss += 0.5 * reg * np.sum(W * W)
+  
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -84,7 +130,12 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  scores[scores>0] = 1
+  scores[np.arange(num_train),y] = -np.sum(scores,axis=1)
+  dW = X.T.dot(scores)
+
+  dW = dW/num_train 
+  dW += reg*W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
